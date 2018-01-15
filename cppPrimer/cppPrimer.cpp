ca093715527624a08830ed4733710976950446a8
danielsbonnin <danielsbonnin@gmail.com>
2018-01-15 11:00:43 -0600

    Stock example

--- cppPrimer/cppPrimer.cpp
+++ cppPrimer/cppPrimer.cpp
@@ -15,11 +15,34 @@
 #include <fstream>
 #include <cstdlib>
 #include <new>
+#include "stocks.h"
 using namespace std;
 
 int main(void)
 {
-	ex9_4();
+	
+	cout.setf(ios_base::fixed, ios_base::floatfield);        // #.## format
+	cout.precision(2);                 // #.## format
+	cout.setf(ios_base::showpoint);    // #.## format
+
+	cout << "Using constructors to create new objects\n";
+	Stock stock1("NanoSmart", 12, 20.0);                     // syntax 1
+	stock1.show();
+	Stock stock2 = Stock("Boffo Objects", 2, 2.0);           // syntax 2
+	stock2.show();
+
+	cout << "Assigning stock1 to stock2:\n";
+	stock2 = stock1;
+	cout << "Listing stock1 and stock2:\n";
+	stock1.show();
+	stock2.show();
+
+	cout << "Using a constructor to reset an object\n";
+	stock1 = Stock("Nifty Foods", 10, 50.0);    // temp object
+	cout << "Revised stock1:\n";
+	stock1.show();
+	cout << "Done\n";
+
 	cin.get();
 	cin.get();
 	return 0;
---
+++ cppPrimer/stocks.h
@@ -0,0 +1,20 @@
+#pragma once
+class Stock   // class declaration
+{
+private:
+	char company[30];
+	int shares;
+	double share_val;
+	double total_val;
+	void set_tot() { total_val = shares * share_val; }
+public:
+	Stock();
+	Stock(const char * co, int n = 0, double pr = 0.0);
+	~Stock();
+	const Stock & topval(const Stock & s) const;
+	void acquire(const char * co, int n, double pr);
+	void buy(int num, double price);
+	void sell(int num, double price);
+	void update(double price);
+	void show();
+};    // note semicolon at the end
