e6fe45a86f6021410135cec0c652a315f3af952f
danielsbonnin <danielsbonnin@gmail.com>
2018-01-20 21:46:10 -0600

    pg 557

--- cppPrimer/cppPrimer.cpp
+++ cppPrimer/cppPrimer.cpp
@@ -19,12 +19,50 @@
 #include <new>
 #include "stocks.h"
 #include "stack.h"
+#include "mytime0.h"
+#include "vect.h"
+
 using namespace std;
 
 int main(void)
 {
+	ofstream fout;
+	fout.open("thewalk.txt");
+	using VECTOR::Vector;
+	srand(time(0));     // seed random-number generator
+	double direction;
+	Vector step;
+	Vector result(0.0, 0.0);
+	unsigned long steps = 0;
+	double target;
+	double dstep;
+	cout << "Enter target distance (q to quit): ";
+	while (cin >> target)
+	{
+		cout << "Enter step length: ";
+		if (!(cin >> dstep))
+			break;
+
+		while (result.magval() < target)
+		{
+			direction = rand() % 360;
+			step.set(dstep, direction, 'p');
+			result = result + step;
+			steps++;
+		}
+		fout << "After " << steps << " steps, the subject "
+			"has the following location:\n";
+		fout << result << endl;
+		result.polar_mode();
+		fout << " or\n" << result << endl;
+		fout << "Average outward distance per step = "
+			<< result.magval() / steps << endl;
+		steps = 0;
+		result.set(0.0, 0.0);
+		cout << "Enter target distance (q to quit): ";
+	}
+	cout << "Bye!\n";
 	
-	ex10_8();
 	cin.get();
 	cin.get();
 	return 0;
---
+++ cppPrimer/mytime0.cpp
@@ -0,0 +1,80 @@
+#include "stdafx.h"
+#include "mytime0.h"
+#include <iostream>
+#include <fstream>
+Time::Time()
+{
+	hours = minutes = 0;
+}
+
+Time::Time(int h, int m)
+{
+	hours = h;
+	minutes = m;
+}
+
+void Time::AddMin(int m)
+{
+	minutes += m;
+	hours += minutes / 60;
+	minutes %= 60;
+}
+
+void Time::AddHr(int h)
+{
+	hours += h;
+}
+
+void Time::Reset(int h, int m)
+{
+	hours = h;
+	minutes = m;
+}
+
+Time Time::operator+(const Time & t) const
+{
+	Time sum;
+	sum.minutes = minutes + t.minutes;
+	sum.hours = hours + t.hours + sum.minutes / 60;
+	sum.minutes %= 60;
+	return sum;
+}
+
+Time Time::operator-(const Time & t) const
+{
+	Time diff;
+	int tot1, tot2;
+	tot1 = t.minutes + 60 * t.hours;
+	tot2 = minutes + 60 * hours;
+	diff.minutes = (tot2 - tot1) % 60;
+	diff.hours = (tot2 - tot1) / 60;
+	return diff;
+}
+
+Time Time::operator*(double mult) const
+{
+	Time result;
+	long totalminutes = hours * mult * 60 + minutes * mult;
+	result.hours = totalminutes / 60;
+	result.minutes = totalminutes % 60;
+	return result;
+}
+
+Time operator*(double m, const Time & t)  // friend not used in definition
+{
+	Time result;
+	long totalminutes = t.hours * m * 60 + t.minutes * m;
+	result.hours = totalminutes / 60;
+	result.minutes = totalminutes % 60;
+	return result;
+}
+
+std::ostream & operator<<(std::ostream & os, const Time & t)
+{
+	os << t.hours << " hours, " << t.minutes << " minutes";
+	return os;
+}
+
+Time::~Time()
+{
+}
