4a784f0eb858f73f5d6a83c7367b33b2abf3ee40
danielsbonnin <danielsbonnin@gmail.com>
2018-02-01 14:58:18 -0600

    pg 670

--- cppPrimer/cppPrimer.cpp
+++ cppPrimer/cppPrimer.cpp
@@ -22,18 +22,31 @@
 #include "mytime0.h"
 #include "vect.h"
 #include "stonewt.h"
+#include "tabtenn0.h"
 
 using namespace std;
 
 int main(void)
 {
-	cout << "One Line: \n";
-	ex12_5();
-
-	cout << "Two lines: \n";
-
-	ex12_6();
-
+	TableTennisPlayer player1("Tara", "Boomdea", false);
+	RatedPlayer rplayer1(1140, "Mallory", "Duck", true);
+	rplayer1.Name();          // derived object uses base method
+	if (rplayer1.HasTable())
+		cout << ": has a table.\n";
+	else
+		cout << ": hasn't a table.\n";
+	player1.Name();           // base object uses base method
+	if (player1.HasTable())
+		cout << ": has a table";
+	else
+		cout << ": hasn't a table.\n";
+	cout << "Name: ";
+	rplayer1.Name();
+	cout << "; Rating: " << rplayer1.Rating() << endl;
+	RatedPlayer rplayer2(1212, player1);
+	cout << "Name: ";
+	rplayer2.Name();
+	cout << "; Rating: " << rplayer2.Rating() << endl;
 	cin.get();
 	cin.get();
 	return 0;
