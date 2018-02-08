00d62de29f3e58843ba628b957f3b2e69cefaec7
danielsbonnin <danielsbonnin@gmail.com>
2018-02-08 17:03:22 -0600

    Listing 14.14

--- cppPrimer/cppPrimer.cpp
+++ cppPrimer/cppPrimer.cpp
@@ -28,50 +28,48 @@
 #include "Brass.h"
 #include "dma.h"
 #include "worker0.h"
+#include "stacktp.h"
 using namespace std;
 
-const int SIZE = 5;
 int main(void)
 {
-	Worker * lolas[SIZE];
-	int ct;
-	for (ct = 0; ct < SIZE; ct++)
+	Stack<string> st;   // create an empty stack
+	char ch;
+	string po;
+	cout << "Please enter A to add a purchase order,\n"
+		<< "P to process a PO, or Q to quit.\n";
+	while (cin >> ch && toupper(ch) != 'Q')
 	{
-		char choice;
-		cout << "Enter the employee category:\n"
-			<< "w: waiter  s: singer  "
-			<< "t: singing waiter  q: quit\n";
-		cin >> choice;
-		while (strchr("wstq", choice) == NULL)
+		while (cin.get() != '\n')
+			continue;
+		if (!isalpha(ch))
 		{
-			cout << "Please enter a w, s, t, or q: ";
-			cin >> choice;
+			cout << '\a';
+			continue;
 		}
-		if (choice == 'q')
-			break;
-		switch (choice)
+		switch (ch)
 		{
-		case 'w':   lolas[ct] = new Waiter;
+		case 'A':
+		case 'a': cout << "Enter a PO number to add: ";
+			cin >> po;
+			if (st.isfull())
+				cout << "stack already full\n";
+			else
+				st.push(po);
 			break;
-		case 's':  lolas[ct] = new Singer;
-			break;
-		case 't':  lolas[ct] = new SingingWaiter;
-			break;
+		case 'P':
+		case 'p': if (st.isempty())
+			cout << "stack already empty\n";
+				  else {
+					  st.pop(po);
+					  cout << "PO #" << po << " popped\n";
+					  break;
+				  }
 		}
-		cin.get();
-		lolas[ct]->Set();
+		cout << "Please enter A to add a purchase order,\n"
+			<< "P to process a PO, or Q to quit.\n";
 	}
-
-	cout << "\nHere is your staff:\n";
-	int i;
-	for (i = 0; i < ct; i++)
-	{
-		cout << endl;
-		lolas[i]->Show();
-	}
-	for (i = 0; i < ct; i++)
-		delete lolas[i];
-	cout << "Bye.\n";
+	cout << "Bye\n";
 	cin.get();
 	cin.get();
 	return 0;
