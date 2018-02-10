cdc766db9b0f83d32a61f17d67cb33d7a98899b4
danielsbonnin <danielsbonnin@gmail.com>
2018-02-10 11:10:29 -0600

    Listing 14.21

--- cppPrimer/cppPrimer.cpp
+++ cppPrimer/cppPrimer.cpp
@@ -34,13 +34,36 @@
 #include "tempmemb.h"
 using namespace std;
 
+template <template <typename T> class Thing>
+class Crab
+{
+private:
+	Thing<int> s1;
+	Thing<double> s2;
+public:
+	Crab() {};
+	// assumes the thing class has push() and pop() members
+	bool push(int a, double x) { return s1.push(a) && s2.push(x); }
+	bool pop(int & a, double & x) { return s1.pop(a) && s2.pop(x); }
+};
+
+
 int main(void)
 {
-	beta<double> guy(3.5, 3);
+	Crab<Stack> nebula;
+// Stack must match template <typename T> class thing
+	int ni;
+	double nb;
+	cout << "Enter int double pairs, such as 4 3.5 (0 0 to end):\n";
+	while (cin >> ni >> nb && ni > 0 && nb > 0)
+	{
+		if (!nebula.push(ni, nb))
+			break;
+	}
 
-	guy.Show();
-	cout << guy.blab(10, 2.3) << endl;
-	cout << "Done\n";
+	while (nebula.pop(ni, nb))
+		cout << ni << ", " << nb << endl;
+	cout << "Done.\n";
 	cin.get();
 	cin.get();
 	return 0;
